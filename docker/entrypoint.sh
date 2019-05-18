57581b2b85fd736c7c77f388c03055a44da0ec1a
Paul G Webster <paul.g.webster@googlemail.com>
2019-05-18 21:29:23 +0100

    Updates to new docker layout

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
+ln -sf /opt/perl5/bin/perl /usr/bin/perl
 shopt -s expand_aliases
 
-mkdir -p /root/perldoc.perl.org/work
-cd /root/perldoc.perl.org/work
+mkdir -p /root/perldoc.perl.org-engine/work
+cd /root/perldoc.perl.org-engine/work
 export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
 rm -Rf output-tmp
 git clone git@github.com:OpusVL/perldoc.perl.org-export.git output-tmp
 cp -fr output-tmp/. output/
 rm -Rf output-tmp
-rm -f /root/perldoc.perl.org/syntax.cache
 
 while true
 do
-    cd /root/perldoc.perl.org
-    git checkout local-dev
+    cd /root/perldoc.perl.org-engine
     perl sitegen.pl
-    cd /root/perldoc.perl.org/work/output
+    cd /root/perldoc.perl.org-engine/work/output
     latest_perl=$(perl -MJSON -MData::Dumper -e 'local $/;open($fh,"<","versions.json");$j=decode_json(<$fh>);print join(".",5,$j->{latest}->{major},$j->{latest}->{minor})')
     ln -sf $latest_perl .default
     git add .
