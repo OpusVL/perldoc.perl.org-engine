b8e6ffb9da89765008260df5222b0d4fce55da09
JohnMica <liviumica@me.com>
2019-06-20 11:15:18 +0100

    removed polifyls from JS, added value to search input

--- Asset/assets/js/main.js
+++ Asset/assets/js/main.js
@@ -1,119 +1,30 @@
 'use strict';
 
-if (typeof Object.assign != 'function') {
-	// Must be writable: true, enumerable: false, configurable: true
-	Object.defineProperty(Object, 'assign', {
-		value: function assign(target, varArgs) {
-			// .length of function is 2
-			'use strict';
-
-			if (target == null) {
-				// TypeError if undefined or null
-				throw new TypeError('Cannot convert undefined or null to object');
-			}
-
-			var to = Object(target);
-
-			for (var index = 1; index < arguments.length; index++) {
-				var nextSource = arguments[index];
-
-				if (nextSource != null) {
-					// Skip over if undefined or null
-					for (var nextKey in nextSource) {
-						// Avoid bugs when hasOwnProperty is shadowed
-						if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
-							to[nextKey] = nextSource[nextKey];
-						}
-					}
-				}
-			}
-			return to;
-		},
-		writable: true,
-		configurable: true
-	});
-}
-// Production steps of ECMA-262, Edition 5, 15.4.4.18
-// Reference: http://es5.github.io/#x15.4.4.18
-if (!Array.prototype.forEach) {
-	Array.prototype.forEach = function(callback /*, thisArg*/) {
-		var T, k;
-
-		if (this == null) {
-			throw new TypeError('this is null or not defined');
-		}
-
-		// 1. Let O be the result of calling toObject() passing the
-		// |this| value as the argument.
-		var O = Object(this);
-
-		// 2. Let lenValue be the result of calling the Get() internal
-		// method of O with the argument "length".
-		// 3. Let len be toUint32(lenValue).
-		var len = O.length >>> 0;
-
-		// 4. If isCallable(callback) is false, throw a TypeError exception.
-		// See: http://es5.github.com/#x9.11
-		if (typeof callback !== 'function') {
-			throw new TypeError(callback + ' is not a function');
-		}
-
-		// 5. If thisArg was supplied, let T be thisArg; else let
-		// T be undefined.
-		if (arguments.length > 1) {
-			T = arguments[1];
-		}
-
-		// 6. Let k be 0.
-		k = 0;
-
-		// 7. Repeat while k < len.
-		while (k < len) {
-			var kValue;
-
-			// a. Let Pk be ToString(k).
-			//    This is implicit for LHS operands of the in operator.
-			// b. Let kPresent be the result of calling the HasProperty
-			//    internal method of O with argument Pk.
-			//    This step can be combined with c.
-			// c. If kPresent is true, then
-			if (k in O) {
-				// i. Let kValue be the result of calling the Get internal
-				// method of O with argument Pk.
-				kValue = O[k];
-
-				// ii. Call the Call internal method of callback with T as
-				// the this value and argument list containing kValue, k, and O.
-				callback.call(T, kValue, k, O);
-			}
-			// d. Increase k by 1.
-			k++;
-		}
-		// 8. return undefined.
-	};
-}
 function navHeight() {
-	var navHeight = document.querySelector('nav').offsetHeight;
+	document.querySelector('.wrapper').style.paddingTop =
+		document.querySelector('nav').offsetHeight + 'px';
+}
 
-	// if (
-	// 	window.innerWidth >
-	// 	document.querySelector('ul.navbar-nav').offsetWidth +
-	// 		document.querySelector('a.navbar-brand').offsetWidth
-	// ) {
-	// 	document.querySelector('nav').classList.add('navbar-expand');
-	// } else {
-	// 	document.querySelector('nav').classList.remove('navbar-expand');
-	// }
-	$('.wrapper').css({
-		'padding-top': navHeight + 'px'
-	});
+var searchFiltered;
+var userInput = document.getElementById('searchinput');
+
+function searchItems() {
+	fetch('/search.json')
+		.then(function(res) {
+			return res.json();
+		})
+		.then(function(data) {
+			searchFiltered = data.filter(function(el) {
+				return el.name.contains(userInput.value);
+			});
+		});
+	console.log(searchFiltered);
 }
 window.addEventListener('DOMContentLoaded', function() {
 	navHeight();
-
 	var menuItems;
 	var latestVersions;
-	function checkMenuItems() {
+	var checkMenuItems = function() {
 		fetch('/versions.json')
 			.then(function(res) {
 				return res.json();
@@ -122,7 +33,7 @@ window.addEventListener('DOMContentLoaded', function() {
 				menuItems = Object.assign({}, data.versions);
 				latestVersions = Object.assign({}, data.latest);
 			});
-	}
+	};
 	checkMenuItems();
 
 	setTimeout(function() {
@@ -162,6 +73,7 @@ window.addEventListener('DOMContentLoaded', function() {
 });
 window.addEventListener('resize', navHeight);
 window.addEventListener('orientationchange', navHeight);
+userInput.addEventListener('input', searchItems);
 
 // create filter function automatically by each letter
 // if more then 2 char and only 1 result go the URL
