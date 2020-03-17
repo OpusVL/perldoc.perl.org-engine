73c6b82096f4d57feb11632ae424774e7582af61
Paul G Webster <paul.g.webster@googlemail.com>
2020-03-17 15:29:05 +0000

    Updated entrypoint to not actually push

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -39,7 +39,7 @@ do
     ln -sf $latest_perl .default
     git add .
     git commit -am "AutoCommit"
-    git push -f origin master
+    #git push -f origin master
     echo "Sleeping for 24 hours before retrying";
     perl -e 'print "Sleeping 24 hours\n"; sleep(60*60*24)'
 done
